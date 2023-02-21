5c138093f5f28d4ff2899ac52444440f2b8be7c0
Ellon M <mordecaimwema11@gmail.com>
2023-02-21 21:41:45 +0300

    create db schema

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE vet_clinic;
 
+CREATE TABLE animals(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INT,
+	neutered BOOLEAN,
+	weight_kg DECIMAL
+);
