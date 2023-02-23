6634348a17f4fdbdc7e5e2c2c7fdf53353f54027
Ellon M <mordecaimwema11@gmail.com>
2023-02-23 14:58:15 +0300

    create DB schema and animals table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE vet_clinic;
 
+CREATE TABLE animals(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INT,
+	neutered BOOLEAN,
+	weight_kg DECIMAL(6, 2)
+);
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(50);
