9c45a436f1b3beb3825dc89b2ba5f204a1016cd0
Ellon M <mordecaimwema11@gmail.com>
2023-02-23 23:15:46 +0300

    added owners and species tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(50) NOT NULL,
+  age INT 
+);
+
+CREATE TABLE species(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
